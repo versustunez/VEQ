53dd82b5460ea2f38b6e384c68f8ccf7aa23e64b
versustunez <mauricegroenwoldt@gmail.com>
2023-11-21 20:07:18 +0100

    Refactor FlatLook UI theme components in plugin
    Enforced variable consistency in FlatLook.cpp, revised drawing of UI components in the theme including buttons, sliders, text areas, tabs and labels. Constant and other non-changing variables have been made constexpr and/or const to improve code readability, optimization and compliance with best practices.

--- Plugin/Source/GUI/Theme/LookAndFeel/FlatLook.cpp
+++ Plugin/Source/GUI/Theme/LookAndFeel/FlatLook.cpp
@@ -13,28 +13,33 @@ FlatLook::~FlatLook() { theme = nullptr; }
 void FlatLook::drawButtonBackground(juce::Graphics &graphics,
                                     juce::Button &button, const juce::Colour &,
                                     bool shouldDrawButtonAsHighlighted, bool) {
-  auto buttonArea = button.getLocalBounds();
+  const auto buttonArea = button.getLocalBounds();
   if (shouldDrawButtonAsHighlighted)
     graphics.setColour(theme->getColor(Theme::Colors::accent).withAlpha(0.3f));
   else
     graphics.setColour(juce::Colours::black.withAlpha(0.2f));
-  graphics.fillRoundedRectangle(buttonArea.getX(), buttonArea.getY(),
-                                buttonArea.getWidth(), buttonArea.getHeight(),
-                                0);
+  graphics.fillRect(buttonArea.getX(), buttonArea.getY(), buttonArea.getWidth(),
+                    buttonArea.getHeight());
 }
 
 void FlatLook::drawRotarySlider(juce::Graphics &g, int x, int y, int width,
                                 int height, float sliderPosProportional,
                                 float rotaryStartAngle, float rotaryEndAngle,
                                 juce::Slider &) {
-  float MAX_RADIAN = 2.53073;
-  float radius = juce::jmin(width / 2.0f, height / 2.0f) - 3.5f;
-  auto centreX = x + width * 0.5f;
-  auto centreY = y + height * 0.5f;
-  float arcPos = sliderPosProportional;
-  auto angle = rotaryStartAngle +
-               sliderPosProportional * (rotaryEndAngle - rotaryStartAngle);
-  float size = 3.5f;
+  constexpr float size = 3.5f;
+  constexpr float MAX_RADIAN = 2.53073;
+
+  const auto widthF = static_cast<float>(width);
+  const auto heightF = static_cast<float>(height);
+  const auto xF = static_cast<float>(x);
+  const auto yF = static_cast<float>(y);
+
+  const float radius = juce::jmin(widthF / 2.0f, heightF / 2.0f) - 3.5f;
+  const auto centreX = xF + widthF * 0.5f;
+  const auto centreY = yF + heightF * 0.5f;
+  const float arcPos = sliderPosProportional;
+  const auto angle = rotaryStartAngle + sliderPosProportional *
+                                            (rotaryEndAngle - rotaryStartAngle);
 
   //---[the real draw]---//
   juce::Path outerArc;
@@ -46,20 +51,22 @@ void FlatLook::drawRotarySlider(juce::Graphics &g, int x, int y, int width,
 
   // prepare pointer for drawing
   juce::Path arc;
-  arc.addCentredArc(
-      0, 0, radius, radius, 0, -MAX_RADIAN,
-      (float)std::clamp(arcPos * 2.0 - 1.0, -1.0, 1.0) * MAX_RADIAN, true);
+  const float radians =
+      std::clamp(arcPos * 2.0f - 1.0f, -1.0f, 1.0f) * MAX_RADIAN;
+  arc.addCentredArc(0, 0, radius, radius, 0, -MAX_RADIAN, radians, true);
   arc.applyTransform(juce::AffineTransform().translated(centreX, centreY));
-  auto colourTwo = theme->getColor(Colors::accentTwo);
-  auto colourMain = theme->getColor(Colors::accent);
+  const auto colourTwo = theme->getColor(Colors::accentTwo);
+  const auto colourMain = theme->getColor(Colors::accent);
   g.setGradientFill(juce::ColourGradient::horizontal(
       colourTwo, centreX - radius, colourMain, centreX + radius));
   g.strokePath(arc, juce::PathStrokeType(size));
 
   juce::Path pointer;
-  auto pointerThickness = 3;
-  auto l = 5;
-  pointer.addEllipse(-pointerThickness * 0.25f, -(radius - l), pointerThickness,
+  constexpr auto pointerThickness = 3;
+  constexpr int radiusOffset = 5;
+  constexpr float pointerPlacement_X = -pointerThickness * 0.25f;
+  float const pointerPlacement_Y = -(radius - radiusOffset);
+  pointer.addEllipse(pointerPlacement_X, pointerPlacement_Y, pointerThickness,
                      pointerThickness);
   pointer.applyTransform(
       juce::AffineTransform::rotation(angle).translated(centreX, centreY));
@@ -69,70 +76,82 @@ void FlatLook::drawRotarySlider(juce::Graphics &g, int x, int y, int width,
 
 void FlatLook::drawTextEditorOutline(juce::Graphics &graphics, int width,
                                      int height, juce::TextEditor &) {
-  graphics.setGradientFill(juce::ColourGradient::horizontal(
-      theme->getColor(Colors::accentTwo), 0, theme->getColor(Colors::accent),
-      (float)width));
-  float p = (float)width / 100.0f * 2.5f;
-  graphics.drawLine(p, (float)height, (float)width - p, (float)height, 1.3f);
+
+  const auto widthF = static_cast<float>(width);
+  const auto heightF = static_cast<float>(height);
+  using juce::ColourGradient;
+  const auto colorOne = theme->getColor(Colors::accentTwo);
+  const auto colorTwo = theme->getColor(Colors::accent);
+
+  graphics.setGradientFill(
+      ColourGradient::horizontal(colorOne, 0, colorTwo, widthF));
+  const float widthOffset = widthF / 100.0f * 2.5f;
+  graphics.drawLine(widthOffset, heightF, widthF - widthOffset, heightF, 1.3f);
 }
 
 void FlatLook::drawToggleButton(juce::Graphics &graphics,
                                 juce::ToggleButton &button, bool,
-                                bool shouldDrawButtonAsDown) {
-  int height = button.getHeight();
-  int width = button.getWidth();
+                                const bool shouldDrawButtonAsDown) {
+  const int height = button.getHeight();
+  const int width = button.getWidth();
   auto text = button.getButtonText();
-  bool isPressed = shouldDrawButtonAsDown ||
-                   button.getToggleStateValue() == button.buttonDown;
+  const bool isPressed =
+      shouldDrawButtonAsDown ||
+      button.getToggleStateValue() == juce::ToggleButton::buttonDown;
+
   if (text == "")
     text = isPressed ? "On" : "Off";
 
-  auto color = theme->getColor(Colors::accent).withAlpha(0.5f);
-  auto color2 = theme->getColor(Colors::accentTwo).withAlpha(0.2f);
-  graphics.setColour(isPressed ? color : color2);
+  const auto onColor = theme->getColor(Colors::accent).withAlpha(0.5f);
+  const auto offColor = theme->getColor(Colors::accentTwo).withAlpha(0.2f);
+  graphics.setColour(isPressed ? onColor : offColor);
 
-  auto l = 0;
-  auto p = 0;
-  graphics.fillRoundedRectangle(l, l, width - p, height - p, 0);
+  constexpr int marginX = 0;
+  constexpr int marginY = 0;
+  constexpr int marginW = marginX * 2;
+  constexpr int marginH = marginY * 2;
+
+  graphics.fillRect(marginX, 0, width - marginW, height);
   graphics.setColour(theme->getColor(Colors::font));
   graphics.setFont(11);
-  graphics.drawText(text, l, l, width - p, height - p,
+  graphics.drawText(text, marginX, marginY, width - marginW, height - marginH,
                     juce::Justification::centred, true);
 }
 
 void FlatLook::drawTabButton(juce::TabBarButton &button,
                              juce::Graphics &graphics, bool, bool) {
-  const juce::Rectangle<int> activeArea(button.getActiveArea());
+  const juce::Rectangle activeArea(button.getActiveArea());
   graphics.setColour(theme->getColor(Colors::bg));
   graphics.fillRect(activeArea);
 
-  int width = button.getWidth();
-  int height = button.getHeight() - 1;
-  auto color = theme->getColor(Colors::accent);
+  const int width = button.getWidth();
+  const int height = button.getHeight() - 1;
+  const auto color = theme->getColor(Colors::accent);
   graphics.setColour(button.getToggleState() ? color : color.withAlpha(0.2f));
-  auto p = 2;
-  graphics.fillRect(0, height - p, width, p);
+  constexpr auto heightOffset = 2;
+  graphics.fillRect(0, height - heightOffset, width, heightOffset);
   graphics.setColour(theme->getColor(Colors::font));
   graphics.drawText(button.getButtonText(), 0, 0, width, height,
                     juce::Justification::centred, true);
 }
 
-void FlatLook::drawComboBox(juce::Graphics &graphics, int width, int height,
-                            bool, int, int, int, int, juce::ComboBox &) {
-  auto borderHeight = 2;
+void FlatLook::drawComboBox(juce::Graphics &graphics, const int width,
+                            const int height, bool, int, int, int, int,
+                            juce::ComboBox &) {
+  constexpr auto borderHeight = 2;
   graphics.setColour(theme->getColor(Colors::accent));
   graphics.fillRect(0, height - borderHeight, width, borderHeight);
   graphics.setColour(theme->getColor(Colors::bgTwo).withAlpha(0.2f));
-  graphics.fillRoundedRectangle(0, 0, width, height - borderHeight, cornerRadius);
+  graphics.fillRect(0, 0, width, height - borderHeight);
 }
 
 void FlatLook::drawLabel(juce::Graphics &graphics, juce::Label &label) {
   if (!label.isBeingEdited()) {
-    auto alpha = label.isEnabled() ? 1.0f : 0.5f;
+    const auto alpha = label.isEnabled() ? 1.0f : 0.5f;
     const juce::Font font(getLabelFont(label));
     graphics.setColour(
         label.findColour(juce::Label::textColourId).withAlpha(alpha));
-    auto textArea =
+    const auto textArea =
         getLabelBorderSize(label).subtractedFrom(label.getLocalBounds());
     graphics.drawFittedText(
         label.getText(), textArea, label.getJustificationType(),
